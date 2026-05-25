82795789988f5fc2da71a1d3d35661dc13a4aeab
dankito <git@dankito.net>
2026-05-25 07:52:09 +0200

    Added Tailwind

--- WebUI/vite.config.ts
+++ WebUI/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE_PATH ?? "/",
   plugins: [
     svelte(),
+    tailwindcss(),
   ],
 })
