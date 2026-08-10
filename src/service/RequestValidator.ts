c643d749de34e8948d2d367b4c10aed992773719
dankito <git@dankito.net>
2026-08-10 21:56:56 +0200

    Better boolean parsing

--- src/service/RequestValidator.ts
+++ src/service/RequestValidator.ts
@@ -21,7 +21,21 @@ export class RequestValidator {
       return ErrorResult.for("Missing required parameter: html")
     }
 
-    return SuccessResult.for(new ExtractFromHtmlQueryParams(html, url, includeMetadata === "true"))
+    return SuccessResult.for(new ExtractFromHtmlQueryParams(html, url, this.parseBoolean(includeMetadata)))
+  }
+
+
+  private parseBoolean(value?: string | boolean): boolean {
+    if (!!!value) {
+      return false
+    }
+
+    if (typeof value === "boolean") {
+      return value
+    }
+
+    const normalized = value.trim().toLowerCase()
+    return normalized === "true" // may use: ['true', '1', 'yes', 'on'].includes(normalized)
   }
 
 
